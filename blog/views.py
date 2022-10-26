1f6364e06bbd7cda94c96c247ac648f52c8128ad
ttroo <kimttroo@gmail.com>
2022-10-26 16:11:52 +0900

    blog.index view에 Post 목록 조회 추가

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
+from blog.models import Post
 
 def index(request):
-    return render(
-        request,
-        "blog/index.html"
-    )
+    # 전체 포스팅을 가져올 준비. 아직 가져오지는 않음.
+    post_qs = Post.objects.all().order_by("-id") # pk = id
+
+    return render(request, "blog/index.html", {
+    "post_list" : post_qs,
+    })
+    # {}는 () 안으로 들어가야 함 = 3번째 인자가 되어야 함!

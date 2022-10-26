095e95a538bd07db0a04fac5f88674d672c46ec2
ttroo <kimttroo@gmail.com>
2022-10-26 14:18:01 +0900

    2022/10/26 blog.Post 모델/마이그레이션 생성

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,12 @@
+from pyexpat import model
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=30)  # 길이 제한 가능
+    content = models.TextField()             # 길이 제한 불가
+    created_at = models.DateTimeField()
+    # author : 추후 작성 예정
+
+
+    def __str__(self):
+        return self.title

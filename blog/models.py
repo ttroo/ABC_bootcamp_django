d375f773e95de84a97becba0b695e11ae3493996
ttroo <kimttroo@gmail.com>
2022-10-27 11:57:14 +0900

    blog.Post 모델에 get_absolute_url 메서드 추가

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,12 @@ class Post(models.Model):
     # author : 추후 작성 예정
 
 
+    def get_absolute_url(self):
+        # TODO : 향후에는 장고의 URL Reverse 기능을 사용하기.
+        return f'/blog/{self.pk}/'
+
+
+
     def __str__(self):
         return f'[{self.pk}]{self.title}'
         
